be5ce1a1bab4dd17d3b27b69e2430ec7eedb0c18
Nickolay.NP <nickolay.np@gmail.com>
2024-03-26 23:11:35 +0300

    v.0.45

--- cpt/cpt.go
+++ cpt/cpt.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
 )
 
 var (
@@ -19,9 +21,15 @@ const (
 )
 
 func main() {
-	var conf config
+	conf := getConfig(os.Args)
 
-	if err := conf.init(os.Args); err != nil {
+	var pw fileProcessor
+	pw.init()
+	pw.pathWalk(conf.sourceFolder)
+}
+
+func getConfig(args []string) (conf config) {
+	if err := conf.init(args); err != nil {
 		switch err.(type) {
 		case printVersion:
 			fmt.Printf("Version: %s (%s)\n", version, date)
@@ -30,12 +38,42 @@ func main() {
 		default:
 			fmt.Fprintf(os.Stderr, "Config error: %v\n", err)
 		}
-		return
+		os.Exit(0)
 	}
-
-	
-
-	// cpt -m ./m1app 24022006.log ../case_2024.02.20/tj/m1app
-	// cpt -m ./ 24022006.log //merope.dept07/csr/error/tj
-
+	return conf
 }
+
+///////////////////////////////////////////////////////////////////////////////
+
+type fileProcessor struct {
+}
+
+func (obj *fileProcessor) init() {
+}
+
+func (obj *fileProcessor) pathWalk(basePath string) {
+	err := filepath.Walk(basePath, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Prevent panic by handling failure accessing a path %q: %v\n", path, err)
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		obj.doProcess(path)
+
+		// if isCancel() {
+		// 	return fmt.Errorf("process is cancel")
+		// }
+
+		return nil
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error walking the path %q: %v\n", basePath, err)
+	}
+}
+
+func (obj *fileProcessor) doProcess(fileName string) {
+}
+
+///////////////////////////////////////////////////////////////////////////////
