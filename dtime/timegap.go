770d1fd2efac93ade068f196cc43b8f6a0ce79d8
Nickolay.NP <nickolay.np@gmail.com>
2024-04-30 12:23:37 +0300

    v.0.53

--- dtime/timegap.go
+++ dtime/timegap.go
@@ -1,18 +1,61 @@
 package main
 
-import "io"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"time"
+)
+
+type timeGapTread struct {
+	process string
+	tread   string
+}
 
 type timeGap struct {
+	treads map[timeGapTread]time.Time
+}
+
+func (obj *timeGap) init() {
+	obj.treads = make(map[timeGapTread]time.Time)
 }
 
 func (obj *timeGap) lineProcessor(data []byte, writer io.Writer) {
+
+	getTread := func(data []byte) []byte {
+		treadMask := []byte(",OSTread=")
+		treadPosition := bytes.Index(data, treadMask)
+		commaPosition := bytes.Index(data[treadPosition+len(treadMask):], []byte(","))
+
+		return data[treadPosition+len(treadMask) : treadPosition+len(treadMask)+commaPosition]
+	}
+	writeEvent := func(f string, args ...any) []byte {
+		return []byte(fmt.Sprintf(f, args...))
+	}
+
 	timeBegin, timeFinish := getStrTimePosition(data)
 	if timeBegin == 0 && timeFinish == 0 {
 		return
 	}
 
-	// strProcess := data[0:timeBegin]
-	// strTime := data[timeBegin:timeFinish]
+	strProcess := data[0:timeBegin]
+	strTime := data[timeBegin:timeFinish]
+	strDuration := getStrDuration(data[timeFinish+1:])
+	strTread := getTread(data)
+
+	thread := timeGapTread{string(strProcess), string(strTread)}
+
+	if eventStartTime, ok := obj.treads[thread]; ok {
+
+		eventStopTime := getStartTime(strTime, strDuration)
+		eventDuration := eventStopTime.Sub(eventStartTime)
+
+		writer.Write(writeEvent("%s%s.%06d-%d,TIMEGAP,OSTread=%s\n",
+			strProcess, eventStopTime.Format("06010215.log:04:05"),
+			eventStopTime.Nanosecond()/1000,
+			eventDuration.Milliseconds(), strTread))
+	}
+	obj.treads[thread] = getTime(strTime)
 
 	writer.Write(data)
 	writer.Write([]byte("\n"))
--- dtime/timegap_test.go
+++ dtime/timegap_test.go
@@ -11,30 +11,35 @@ func Test_timeGap_lineProcessor(t *testing.T) {
 		data       []byte
 		wantWriter string
 	}{
-		{"test 1", []byte(""), "\n"},
+		{"test 1", []byte(""), ""},
 		{"test 2", []byte(`.\rphost_2345\24011215.log:32:47.733007-0,EXCP,OSTread=1,
 .\rphost_2345\24011215.log:22:47.733007-0,EXCP,OSTread=2,
-.\rphost_2346\24011215.log:42:47.733007-0,EXCP,OSTread=1,
+.\rphost_2346\24011215.log:34:47.733007-0,EXCP,OSTread=1,
 .\rphost_2345\24011215.log:35:00.000001-0,EXCP,OSTread=1,
 .\rphost_2345\24011215.log:35:00.003003-0,EXCP,OSTread=2,
 .\rphost_2346\24011215.log:35:00.003003-1000,EXCP,OSTread=1,`),
 			`.\rphost_2345\24011215.log:32:47.733007-0,EXCP,OSTread=1,
 .\rphost_2345\24011215.log:22:47.733007-0,EXCP,OSTread=2,
-.\rphost_2346\24011215.log:42:47.733007-0,EXCP,OSTread=1,
-.\rphost_2345\24011215.log:35:00.000001-10000,TIMEGAP,OSTread=1,
+.\rphost_2346\24011215.log:34:47.733007-0,EXCP,OSTread=1,
+.\rphost_2345\24011215.log:35:00.000001-132266,TIMEGAP,OSTread=1
 .\rphost_2345\24011215.log:35:00.000001-0,EXCP,OSTread=1,
-.\rphost_2345\24011215.log:35:00.000001-10000,TIMEGAP,OSTread=2,
+.\rphost_2345\24011215.log:35:00.003003-732269,TIMEGAP,OSTread=2
 .\rphost_2345\24011215.log:35:00.003003-0,EXCP,OSTread=2,
-.\rphost_2346\24011215.log:35:00.000001-10000,TIMEGAP,OSTread=1,
-.\rphost_2346\24011215.log:35:00.003003-1000,EXCP,OSTread=1,`},
+.\rphost_2346\24011215.log:35:00.002003-12268,TIMEGAP,OSTread=1
+.\rphost_2346\24011215.log:35:00.003003-1000,EXCP,OSTread=1,
+`},
 	}
 
 	var obj timeGap
+	obj.init()
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			writer := &bytes.Buffer{}
-			obj.lineProcessor(tt.data, writer)
+			splitData := bytes.Split(tt.data, []byte("\n"))
+			for i := range splitData {
+				obj.lineProcessor(splitData[i], writer)
+			}
 			if gotWriter := writer.String(); gotWriter != tt.wantWriter {
 				t.Errorf("timeGap.lineProcessor() = %v, want %v", gotWriter, tt.wantWriter)
 			}
